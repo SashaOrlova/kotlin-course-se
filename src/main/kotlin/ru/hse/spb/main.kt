7fe9ab6b385f74ec9809a67e5a9a3f0ea9238288
SashaOrlova <sasha.orlova.1998n@mail.ru>
2018-09-11 20:23:56 +0300

    add first version of solution

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,71 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+const val ADJECTIVE_M = "lios"
+const val ADJECTIVE_W = "liala"
+const val VERB_M = "initis"
+const val VERB_W = "inites"
+const val NOUN_M = "etr"
+const val NOUN_W = "etra"
 
-    return words.joinToString(separator = " ")
+val cases = mutableListOf(ADJECTIVE_M, ADJECTIVE_W, NOUN_M, NOUN_W, VERB_M, VERB_W)
+enum class Gender {
+    MAN, WOMAN, UNKNOWN
+}
+
+fun checkOneWord(word: String, kind: String): Boolean {
+    return word.endsWith(kind)
+}
+
+fun getGender(word: String): Gender {
+    var gender = Gender.UNKNOWN
+    if (checkOneWord(word, ADJECTIVE_M))
+        gender = Gender.MAN
+    if (checkOneWord(word, ADJECTIVE_W))
+        gender = Gender.WOMAN
+    if (gender == Gender.UNKNOWN) {
+        if (checkOneWord(word, NOUN_M))
+            gender = Gender.MAN
+        if (checkOneWord(word, NOUN_W))
+            gender = Gender.WOMAN
+    }
+    return gender
+}
+
+fun checkWithGender(sentence: List<String>, adjective: String, noun: String, verb: String): Boolean {
+    var pos = 0
+    while (pos < sentence.size && checkOneWord(sentence[pos], adjective))
+        pos++
+    if (pos == sentence.size || pos < sentence.size && !checkOneWord(sentence[pos], noun))
+        return false
+    pos++
+    while (pos < sentence.size && checkOneWord(sentence[pos], verb))
+        pos++
+    if (pos == sentence.size)
+        return true
+    return false
+}
+
+fun checkPetersLanguage(sentence: List<String>): Boolean {
+    if (sentence.size == 1) {
+        var answer = false
+        for (kind in cases) {
+            answer = answer || checkOneWord(sentence[0], kind)
+        }
+        return answer
+    }
+    val gender = getGender(sentence[0])
+    return when (gender) {
+        Gender.UNKNOWN -> false
+        Gender.MAN -> checkWithGender(sentence, ADJECTIVE_M, NOUN_M, VERB_M)
+        Gender.WOMAN -> checkWithGender(sentence, ADJECTIVE_W, NOUN_W, VERB_W)
+    }
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val words = readLine()!!.split(' ')
+    val result = checkPetersLanguage(words)
+    if (result)
+        println("YES")
+    else
+        println("NO")
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,40 @@
 package ru.hse.spb
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.Assert.*
 
-class TestSource {
+class ProblemAKtTest {
+
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testCheckOneWord() {
+        assertTrue(checkOneWord("petr", NOUN_M))
+        assertTrue(checkOneWord("nataliala", ADJECTIVE_W))
+        assertTrue(checkOneWord("feinites", VERB_W))
+        assertFalse(checkOneWord("etis", NOUN_W))
+        assertFalse(checkOneWord("kataliala", VERB_W))
+        assertFalse(checkOneWord("inites", ADJECTIVE_M))
+    }
+
+    @Test
+    fun testGetGender() {
+        assertEquals(getGender("petra"), Gender.WOMAN)
+        assertEquals(getGender("initas"), Gender.UNKNOWN)
+        assertEquals(getGender("nuiteretr"), Gender.MAN)
+    }
+
+    @Test
+    fun testCheckWithGender() {
+        assertTrue(checkWithGender("nataliala kataliala vetra feinites".split(' '),
+                ADJECTIVE_W, NOUN_W, VERB_W))
+        assertFalse(checkWithGender("etis atis animatis etis atis amatis".split(' '),
+                ADJECTIVE_M, NOUN_M, VERB_M))
+    }
+
+    @Test
+    fun testCheckPetersLanguage() {
+        assertTrue(checkPetersLanguage(listOf("qweasbvflios")))
+        assertTrue(checkPetersLanguage("liala petra inites".split(' ')))
+        assertFalse(checkPetersLanguage("petra petra petra".split(' ')))
+        assertFalse(checkPetersLanguage("pliala petr".split(' ')))
     }
 }
